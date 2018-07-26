b8ad89f88f74bb396aaf4297720a3ac048867ff1
George Mauer <gmauer@gmail.com>
2018-07-26 16:17:10 -0500

    onClick

--- src/StarRating.js
+++ src/StarRating.js
@@ -3,35 +3,35 @@ import styled from 'styled-components'
 
 const repeat = times => fn => Array(times).fill(0).map(fn)
 const isNonZeroFalsy = v => !v && v !== 0
+const noop = () => {}
 
-const Star = styled(({className}) => (
-    <div className={className}>★</div>
+const Star = styled(({className, onClick}) => (
+    <div className={className} onClick={onClick}>★</div>
 ))`
     font-size: 50px;
 `
-const renderStar = ({className}) => <Star className={className} />
-const UnselectedStar = styled(renderStar)`
+const UnselectedStar = styled(Star)`
     color: gold;
 `
-const SelectedStar = styled(renderStar)`
+const SelectedStar = styled(Star)`
     color: green;
 `
-const InactiveStar = styled(renderStar)`
+const InactiveStar = styled(Star)`
     color: gray;
 `
 
-const getStar = (value, index) => React.createElement(
+const getStar = (value, starValue, onClick) => React.createElement(
     isNonZeroFalsy(value) ? InactiveStar :
-    (index < value)       ? SelectedStar
+    (starValue <= value)  ? SelectedStar
                           : UnselectedStar,
-    {key: `star${index}`}
+    {key: `star${starValue}`, onClick: event => onClick({starValue, event})}
 )
 
-const StarRating = styled(({starCount=5, value=5, className}) => (
+export const StarRating = styled(({starCount=5, value=5, className, onClick=noop}) => (
     <div className={className}>
         <input type="range" min="0" max={starCount} step="1" value={isNonZeroFalsy(value) ? '' : value} />
         <React.Fragment>
-            {repeat(starCount)((_, i) => getStar(value, i))}
+            {repeat(starCount)((_, i) => getStar(value, i+1, onClick))}
         </React.Fragment>
     </div>
 ))`
--- src/stories/index.js
+++ src/stories/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { withKnobs, number } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 
 import StarRating from '../StarRating.js'
 
@@ -10,5 +11,6 @@ storiesOf(`Star System`, module)
     .add(`Star rating`, () => (
         <StarRating
             starCount={number(`Star Count`, 5)}
-            value={number(`Value`, 3)} />
+            value={number(`Value`, 3)}
+            onClick={action(`Star click`)} />
     ))
