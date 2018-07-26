7587fdf3e139e5106d4bdc78717f093cbacbb106
George Mauer <gmauer@gmail.com>
2018-07-26 15:59:19 -0500

    render stars at initial value

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,13 +1,44 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const repeat = times => fn => Array(times).fill(0).map(fn)
+const isNonZeroFalsy = v => !v && v !== 0
 
-const Star = () => `★`
+const Star = styled(({className}) => (
+    <div className={className}>★</div>
+))`
+    font-size: 50px;
+`
+const renderStar = ({className}) => <Star className={className} />
+const UnselectedStar = styled(renderStar)`
+    color: gold;
+`
+const SelectedStar = styled(renderStar)`
+    color: green;
+`
+const InactiveStar = styled(renderStar)`
+    color: gray;
+`
 
-const StarRating = ({starCount = 5}) => (
-    React.createElement(`div`, null, ...repeat(starCount)(() => (
-        <Star />
-    )))
+const getStar = (value, index) => React.createElement(
+    isNonZeroFalsy(value) ? InactiveStar :
+    (index < value)       ? SelectedStar
+                          : UnselectedStar,
+    {key: `star${index}`}
 )
+
+const StarRating = styled(({starCount=5, value=5, className}) => (
+    <div className={className}>
+        <input type="range" min="0" max={starCount} step="1" value={isNonZeroFalsy(value) ? '' : value} />
+        <React.Fragment>
+            {repeat(starCount)((_, i) => getStar(value, i))}
+        </React.Fragment>
+    </div>
+))`
+    display: inline-flex;
+    & > input {
+      display: none;
+    }
+`
 
 export default StarRating
--- src/stories/index.js
+++ src/stories/index.js
@@ -8,5 +8,7 @@ import StarRating from '../StarRating.js'
 storiesOf(`Star System`, module)
     .addDecorator(withKnobs)
     .add(`Star rating`, () => (
-        <StarRating starCount={number(`Star Count`, 5)}/>
+        <StarRating
+            starCount={number(`Star Count`, 5)}
+            value={number(`Value`, 3)} />
     ))
